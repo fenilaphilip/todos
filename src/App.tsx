07b905e63534d2518ff2268b55816bea7d8a219d
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-06-20 15:12:14 +0200

    Removing todo by clicking the todoitem

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,16 @@ function App() {
     });
   };
 
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,11 +3,17 @@ import Todo from "../dataModel/todo.ts";
 import TodoItem from "./TodoItem.tsx";
 import classes from "../styles/Todos.module.css";
 
-const Todos: FC<{ items: Todo[] }> = (props) => {
+const Todos: FC<{ items: Todo[]; onRemoveTodo: (todoId: string) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
