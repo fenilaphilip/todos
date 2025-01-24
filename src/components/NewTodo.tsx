06ad90de4e0d1ab0cd7b0a052de15ff88ece0bb6
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-01-24 12:16:27 +0100

    New structural design is added

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -21,16 +21,63 @@ const NewTodo: React.FC = () => {
   return (
     <>
       <div className={classes.appname}>Todos</div>
-      <form onSubmit={submitHandler} className={classes.form}>
-        <label>Task name:</label>
-        <input
-          type="text"
-          id="text"
-          ref={todoTextInputRef}
-          placeholder="What to do..........??"
-        />
-        <button>Add Todo</button>
-      </form>
+      <div className={classes.newTodoContainer}>
+        <div className={classes.createtodo}>Create To do</div>
+
+        <form onSubmit={submitHandler} className={classes.form}>
+          <label>Task name:</label>
+          <input
+            type="text"
+            id="newTaskInput"
+            ref={todoTextInputRef}
+            placeholder="What to do..........??"
+            className={classes.newInput}
+          />
+          <div className={classes.formarea}>
+            <div>
+              <label>More info:</label>
+              <textarea
+                id="inputMoreInfo"
+                placeholder="Enter more details......."
+                className={classes.moreinfo}
+              />
+            </div>
+            <div>
+              <label className={classes.prioritylabel}>Set Priority:</label>
+              <div className={classes.prioritylevel}>
+                <input
+                  type="radio"
+                  id="lowPriority"
+                  name="priority"
+                  value="low"
+                />
+                <label htmlFor="lowPriority">Low</label>
+                <input
+                  type="radio"
+                  id="mediumPriority"
+                  name="priority"
+                  value="mediumPriority"
+                />
+                <label htmlFor="medium">Medium</label>
+                <input
+                  type="radio"
+                  id="highPriority"
+                  name="priority"
+                  value="high"
+                />
+                <label htmlFor="high">High</label>
+              </div>
+              <br />
+              <br />
+              <label>When you should finish this task?</label>
+              <input type="date" id="deadlineInput" placeholder="" />
+              <br />
+              <button>Add Todo</button>
+              <button>Reset Todo</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
