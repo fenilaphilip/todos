6c70c6e935b98ce58f512960bcabe2323b7b4338
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-02-21 10:36:47 +0100

    NewTodo and Todos hiding for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import NewTodo from "./components/NewTodo.tsx";
+// import NewTodo from "./components/NewTodo.tsx";
 import Navbar from "./components/Navbar.tsx";
-import Todos from "./components/Todos";
+// import Todos from "./components/Todos";
 import TodosContextProvider from "./store/todos-context.tsx";
 
 function App() {
   return (
     <TodosContextProvider>
       <Navbar />
-      <NewTodo />
-      <Todos />
+      {/* <NewTodo />
+      <Todos /> */}
     </TodosContextProvider>
   );
 }
