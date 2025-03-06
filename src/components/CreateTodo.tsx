49582cac79eff562de8426f0e5faccb6967acc49
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-06 16:20:01 +0100

    Adding small input component

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { Box, TextField, Button } from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import { Box, TextField, IconButton, InputAdornment } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 const CreateTodo: React.FC = () => {
   return (
     <>
       <Box>
-        <Grid container spacing={2}>
-          <Grid size={{ xs: 12, sm: 9, md: 10 }}>
-            <TextField fullWidth />
-          </Grid>
-          <Grid size={{ xs: 12, sm: 3, md: 2 }}>
-            <Button>Done</Button>
-          </Grid>
-        </Grid>
+        <TextField
+          fullWidth
+          placeholder="Enter a new task"
+          margin="normal"
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton>
+                    <AddIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
       </Box>
     </>
   );
