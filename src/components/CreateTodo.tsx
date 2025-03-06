af33af1c72719076db59a17a99b5614a2140073b
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-06 17:41:45 +0100

    Taking Input from user and adding it on the todos

--- src/components/CreateTodo.tsx
+++ src/components/CreateTodo.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
 import { Box, TextField, IconButton, InputAdornment } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import Todo, { Priority } from "../dataModel/todo";
+import { addTodo } from "../store/todoSlice";
+import uniqid from "uniqid";
 
 const CreateTodo: React.FC = () => {
+  const taskCaption = useRef<HTMLInputElement>(null);
+  const dispatch = useDispatch();
+
+  const addTodoHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const caption = taskCaption.current!.value;
+    if (caption.trim().length === 0) {
+      alert("Please enter a task");
+      return;
+    }
+
+    const newTask: Todo = {
+      id: uniqid(),
+      description: "",
+      dueDate: undefined,
+      completed: false,
+      caption: caption,
+      priority: Priority.Medium,
+    };
+
+    dispatch(addTodo(newTask));
+  };
+
   return (
     <>
       <Box>
@@ -10,11 +38,12 @@ const CreateTodo: React.FC = () => {
           fullWidth
           placeholder="Enter a new task"
           margin="normal"
+          inputRef={taskCaption}
           slotProps={{
             input: {
               startAdornment: (
                 <InputAdornment position="start">
-                  <IconButton>
+                  <IconButton onClick={addTodoHandler}>
                     <AddIcon />
                   </IconButton>
                 </InputAdornment>
