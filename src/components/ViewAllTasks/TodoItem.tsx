f880793828272f3910d9425110dc972a5511fe91
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-06 12:25:18 +0100

    Renamed and functionality added

--- src/components/ViewAllTasks/TodoItem.tsx
+++ src/components/ViewAllTasks/TodoItem.tsx
@@ -1,30 +1,73 @@
 import React from "react";
-import Todo from "../../dataModel/todo";
-import classes from "../../styles/TodoItem.module.css";
 import { useDispatch } from "react-redux";
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Checkbox,
+  Input,
+  TextField,
+  MenuItem,
+  Button,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Grid from "@mui/material/Grid2";
+import Todo from "../../dataModel/todo";
 import { deleteTodo } from "../../store/todoSlice";
-import { Priority } from "../../dataModel/todo";
 
 const TodoItem: React.FC<{ todo: Todo }> = ({ todo }) => {
   const dispatch = useDispatch();
-
-  let dotClass = classes.dotForLow;
-  if (todo.priority == Priority.Medium) {
-    dotClass = classes.dotForMedium;
-  } else if (todo.priority == Priority.High) {
-    dotClass = classes.dotForHigh;
-  }
-
   return (
-    <li className={classes.item}>
-      <span className={dotClass}></span>
-      <span className={classes.caption}>{todo.caption}</span>
-      <span className={classes.dueDate}>
-        {todo.dueDate ? new Date(todo.dueDate).toLocaleDateString() : ""}
-      </span>
-      <p>{todo.description}</p>
-      <button onClick={() => dispatch(deleteTodo(todo.id))}>Delete</button>
-    </li>
+    <>
+      <Box>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Checkbox />
+            <Input fullWidth value={todo.caption} />
+          </AccordionSummary>
+          <AccordionDetails>
+            <Grid container spacing={2}>
+              <Grid size={{ xs: 12, sm: 12, md: 6 }}>
+                <TextField
+                  fullWidth
+                  label="Notes"
+                  multiline
+                  rows={8}
+                  value={todo.description}
+                />
+              </Grid>
+              <Grid size={{ xs: 12, sm: 12, md: 6 }}>
+                <Grid container spacing={2}>
+                  <Grid size={{ xs: 12, sm: 12, md: 6 }}>
+                    <TextField fullWidth label="datepicker" />
+                  </Grid>
+                  <Grid size={{ xs: 12, sm: 12, md: 6 }}>
+                    <TextField fullWidth label="Priority" select>
+                      <MenuItem>None</MenuItem>
+                      <MenuItem>Low</MenuItem>
+                      <MenuItem>Medium</MenuItem>
+                      <MenuItem>High</MenuItem>
+                    </TextField>
+                  </Grid>
+                  <Grid size={{ xs: 12, sm: 12, md: 12 }}>
+                    <TextField fullWidth label="LabelSelect" />
+                  </Grid>
+                  <Grid size={{ xs: 12, sm: 12, md: 12 }}>
+                    <Button
+                      variant="outlined"
+                      onClick={() => dispatch(deleteTodo(todo.id))}
+                    >
+                      Delete
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
+    </>
   );
 };
 
