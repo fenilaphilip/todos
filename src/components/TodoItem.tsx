ec3166885ee48c283a4f39f40d59ad5101613e22
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-06 18:47:07 +0100

    Added  values and save change button

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import {
   Box,
@@ -10,23 +10,40 @@ import {
   TextField,
   MenuItem,
   Button,
+  Stack,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Grid from "@mui/material/Grid2";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import Todo from "../dataModel/todo";
-import { deleteTodo } from "../store/todoSlice";
+import Todo, { Priority } from "../dataModel/todo";
+import { deleteTodo, editTodo } from "../store/todoSlice";
 
 const TodoItem: React.FC<{ todo: Todo }> = ({ todo }) => {
+  const [todoUpdate, setTodoUpdate] = useState<Todo>({
+    id: todo.id,
+    description: todo.description,
+    dueDate: todo.dueDate,
+    completed: todo.completed,
+    caption: todo.caption,
+    priority: todo.priority,
+  });
+
   const dispatch = useDispatch();
+
   return (
     <Box>
       <Accordion>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Checkbox />
-          <Input fullWidth value={todo.caption} />
+          <Input
+            fullWidth
+            value={todoUpdate.caption}
+            onChange={(e) =>
+              setTodoUpdate({ ...todoUpdate, caption: e.target.value })
+            }
+          />
         </AccordionSummary>
         <AccordionDetails>
           <Grid container spacing={2}>
@@ -36,7 +53,10 @@ const TodoItem: React.FC<{ todo: Todo }> = ({ todo }) => {
                 label="Notes"
                 multiline
                 rows={8}
-                value={todo.description}
+                value={todoUpdate.description}
+                onChange={(e) =>
+                  setTodoUpdate({ ...todoUpdate, description: e.target.value })
+                }
               />
             </Grid>
             <Grid size={{ xs: 12, sm: 12, md: 6 }}>
@@ -50,23 +70,42 @@ const TodoItem: React.FC<{ todo: Todo }> = ({ todo }) => {
                   </LocalizationProvider>
                 </Grid>
                 <Grid size={{ xs: 12, sm: 12, md: 6 }}>
-                  <TextField fullWidth label="Priority" select>
-                    <MenuItem>None</MenuItem>
-                    <MenuItem>Low</MenuItem>
-                    <MenuItem>Medium</MenuItem>
-                    <MenuItem>High</MenuItem>
+                  <TextField
+                    fullWidth
+                    label="Set Priority"
+                    select
+                    value={todoUpdate.priority}
+                    onChange={(e) =>
+                      setTodoUpdate({
+                        ...todoUpdate,
+                        priority: parseInt(e.target.value),
+                      })
+                    }
+                  >
+                    <MenuItem value={Priority.None}>None</MenuItem>
+                    <MenuItem value={Priority.Low}>Low</MenuItem>
+                    <MenuItem value={Priority.Medium}>Medium</MenuItem>
+                    <MenuItem value={Priority.High}>High</MenuItem>
                   </TextField>
                 </Grid>
                 <Grid size={{ xs: 12, sm: 12, md: 12 }}>
                   <TextField fullWidth label="LabelSelect" />
                 </Grid>
                 <Grid size={{ xs: 12, sm: 12, md: 12 }}>
-                  <Button
-                    variant="outlined"
-                    onClick={() => dispatch(deleteTodo(todo.id))}
-                  >
-                    Delete
-                  </Button>
+                  <Stack spacing={2} direction="row">
+                    <Button
+                      variant="outlined"
+                      onClick={() => dispatch(editTodo(todoUpdate))}
+                    >
+                      Save Changes
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      onClick={() => dispatch(deleteTodo(todo.id))}
+                    >
+                      Delete
+                    </Button>
+                  </Stack>
                 </Grid>
               </Grid>
             </Grid>
--- src/dataModel/todo.ts
+++ src/dataModel/todo.ts
@@ -1,5 +1,6 @@
 
 export enum Priority {
+    None,
     Low,
     Medium,
     High
