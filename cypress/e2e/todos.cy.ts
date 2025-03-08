dd5bfceabc9d7e10721f0e89fb9c49fe986eb98f
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-08 14:31:01 +0100

    Testing current application navigation , got successfull

--- cypress/e2e/todos.cy.ts
+++ cypress/e2e/todos.cy.ts
@@ -1,55 +1,67 @@
+function loadApp() {
+  cy.visit('/');
+  cy.get('nav[aria-label="Desktop"]>ul>li').as('navitems');
+}
+
 describe('Testing Todos app', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
+  beforeEach(loadApp);
 
   context('TopNavbar', () => {
-    it(`Contains App name 'Todos'`, () => {
-      cy.get('[data-cy="topNav"]').contains('Todos');
-    })
+    it(`Contains App name 'TODOS'`, () => {
+      cy.get('.MuiStack-root > .MuiTypography-root').contains('TODOS');
+    });
   });
 
   context('SideNavbar', () => {
-    it(`Contains 'Create New Todo' element `, () => {
-      cy.get('[data-cy="sideNav"]').children()
-        .contains('Create new Task')
-        .should('have.attr', 'href')
-        .and('equal', '/create');
+    it(`Contains 'Task Bucket' element to view all tasks`, () => {
+      cy.get('@navitems').find('a[href="/allTasks"]')
+        .contains('Task Bucket');
     });
 
-    it(`Contains 'View All Tasks' element`, () => {
-      cy.get('[data-cy="sideNav"]').children()
-        .contains('View All Tasks')
-        .should('have.attr', 'href')
-        .and('equal', '/todos');
+    it(`Contains 'Calender View' element to view tasks ordered by deadline`, () => {
+      cy.get('@navitems').find('a[href="/calenderView"]')
+        .contains('Calender View');
+    });
+
+    it(`Contains 'Completed Tasks' to show tasks which are completed`, () => {
+      cy.get('@navitems').find('a[href="/completedTasks"]')
+        .contains('Completed Tasks');
+    });
+
+    it(`Allows users to navigate to 'Task Bucket' page`, () => {
+      cy.get('@navitems').children()
+        .contains('Task Bucket').click();
+      cy.url().should('include', '/allTasks');
+    });
+
+    it(`Allows users to navigate to 'Calender View' page`, () => {
+      cy.get('@navitems').children()
+        .contains('Calender View').click();
+      cy.url().should('include', '/calenderView');
     })
 
-    it(`Allows users to navigate to create page`, () => {
-      cy.get('[data-cy="sideNav"]').children()
-        .contains('Create new Task').click();
-      cy.url().should('include', '/create');
-    });
-
-    it(`Allows users to navigate to todos page`, () => {
-      cy.get('[data-cy="sideNav"]').children()
-        .contains('View All Tasks').click();
-      cy.url().should('include', '/todos');
+    it(`Allows users to navigate to 'Completed Tasks' page`, () => {
+      cy.get('@navitems').children()
+        .contains('Completed Tasks').click();
+      cy.url().should('include', '/completedTasks');
     })
   });
 });
 
 
 describe('Create Page', () => {
-  beforeEach(() => {
-    cy.visit('/create');
-  });
-
+  beforeEach(loadApp);
   context('Creating new Todo', () => {
-    it(`Accepts form inputs`, () => {
-      cy.get('[data-cy="CreateTodoForm"]').find("#newTaskInput")
+    it(`Can create a simple todo`, () => {
+      // When 
+      cy.get('[data-cy="create-todo-input-caption"]')
         .type('Watch cricket match with friends');
-      cy.get('[data-cy="CreateTodoForm"]').find("#inputMoreInfo")
-        .type('beer, pizza, popcorn');
+      cy.get('[data-cy="create-todo-button-add"]').click();
+      // Then
+      cy.get('[data-cy="todo-items"]').children()
+        .find('.todo-item-caption > input')
+        .should('have.value', 'Watch cricket match with friends');
     });
   });
 });
+
