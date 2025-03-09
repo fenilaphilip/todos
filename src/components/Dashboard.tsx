51a625bd12a624fb2f5edc97ebfb4b2a764a9a10
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-09 18:39:12 +0100

    Reuseable Dashboard component created

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { Button } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import CreateTodo from "./CreateTodo";
+import Todo from "../dataModel/todo";
+import TodoItem from "./TodoItem";
+
+const Dashboard: React.FC<{
+  completedtaskCount: number;
+  undoneTasks: Todo[];
+  alltasks: Todo[];
+}> = ({ completedtaskCount, undoneTasks, alltasks }) => {
+  const [hideCompletedTask, setHideCompletedTask] = useState(true);
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, sm: 9, md: 10 }}>
+          <CreateTodo />
+        </Grid>
+        <Grid container size={{ xs: 12, sm: 3, md: 2 }}>
+          <Button
+            fullWidth
+            variant="text"
+            startIcon={
+              hideCompletedTask ? <VisibilityOffIcon /> : <VisibilityIcon />
+            }
+            onClick={() => {
+              setHideCompletedTask(!hideCompletedTask);
+            }}
+          >
+            Done ({completedtaskCount})
+          </Button>
+        </Grid>
+      </Grid>
+      {hideCompletedTask && (
+        <div data-cy="todo-items">
+          {undoneTasks.map((todo) => {
+            return <TodoItem key={todo.id} todo={todo} />;
+          })}
+        </div>
+      )}
+      {!hideCompletedTask && (
+        <div data-cy="todo-items">
+          {alltasks.map((todo) => {
+            return <TodoItem key={todo.id} todo={todo} />;
+          })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Dashboard;
