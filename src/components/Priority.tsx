416d12fc40d3a4afa8907fe50ecd058e296675fe
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-10 15:41:10 +0100

    eliminating done task from priority

--- src/components/Priority.tsx
+++ src/components/Priority.tsx
@@ -7,7 +7,7 @@ import { Priority } from "../dataModel/todo";
 export const HighPriority: React.FC = () => {
   const todolist = useSelector((state: RootState) => state);
   const highPriorityTodo = todolist.filter(
-    (todo) => todo.priority === Priority.High
+    (todo) => todo.priority === Priority.High && todo.completed === false
   );
 
   return (
@@ -22,7 +22,7 @@ export const HighPriority: React.FC = () => {
 export const MediumPriority: React.FC = () => {
   const todolist = useSelector((state: RootState) => state);
   const mediumPriorityTodo = todolist.filter(
-    (todo) => todo.priority === Priority.Medium
+    (todo) => todo.priority === Priority.Medium && todo.completed === false
   );
 
   return (
@@ -37,7 +37,7 @@ export const MediumPriority: React.FC = () => {
 export const LowPriority: React.FC = () => {
   const todolist = useSelector((state: RootState) => state);
   const lowPriorityTodo = todolist.filter(
-    (todo) => todo.priority === Priority.Low
+    (todo) => todo.priority === Priority.Low && todo.completed === false
   );
 
   return (
@@ -52,7 +52,7 @@ export const LowPriority: React.FC = () => {
 export const NonePriority: React.FC = () => {
   const todolist = useSelector((state: RootState) => state);
   const nonePriorityTodo = todolist.filter(
-    (todo) => todo.priority === Priority.None
+    (todo) => todo.priority === Priority.None && todo.completed === false
   );
 
   return (
