58052a883994c0e79ee4071ad42b859e7dcd18f1
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-04-04 10:51:44 +0200

    Replacing with utiliy component label

---
+++ src/components/filters/Labels.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import type { RootState } from "../../store/todoStore";
+import { useSelector } from "react-redux";
+import Dashboard from "../utils/Dashboard";
+import { Chip } from "@mui/material";
+
+export const ActiveTaskCount: React.FC<{ labelName: string }> = ({
+  labelName,
+}) => {
+  const todolist = useSelector((state: RootState) => state);
+  const catergoryTodo = todolist.filter((todo) =>
+    todo.labels?.includes(`${labelName}`)
+  );
+  const taskUncompleted = catergoryTodo.filter(
+    (todo) => todo.completed === false
+  );
+
+  return <Chip label={taskUncompleted.length} size="small" color="primary" />;
+};
+
+export const Labels: React.FC<{ labelName: string }> = ({ labelName }) => {
+  const todolist = useSelector((state: RootState) => state);
+  const catergoryTodo = todolist.filter((todo) =>
+    todo.labels?.includes(`${labelName}`)
+  );
+  const taskUncompleted = catergoryTodo.filter(
+    (todo) => todo.completed === false
+  );
+  const completedtaskCount = catergoryTodo.length - taskUncompleted.length;
+
+  return (
+    <Dashboard
+      labelName={labelName}
+      completedtaskCount={completedtaskCount}
+      undoneTasks={taskUncompleted}
+      alltasks={catergoryTodo}
+      showLabel={false}
+      showDuedate={true}
+    />
+  );
+};
