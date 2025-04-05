6d84e32702fa6c179971a0bfa989b45b3ecfe025
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-04-05 21:22:42 +0200

    label can add by pressing enter

--- src/components/settings/EditLabels.tsx
+++ src/components/settings/EditLabels.tsx
@@ -21,11 +21,20 @@ import {
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
+import useKey from "@rooks/use-key";
 
 const EditLabels: React.FC = () => {
   const labelsArray = useSelector((state: RootState) => state.LABELS);
   const newlabel = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch();
+
+  useKey(["Enter"], windowEnter, {
+    target: newlabel,
+  });
+
+  function windowEnter() {
+    handleAddLabel();
+  }
 
   const handleAddLabel = () => {
     const label = newlabel.current!.value;
@@ -39,54 +48,56 @@ const EditLabels: React.FC = () => {
   };
 
   return (
-    <Paper>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Label Name</TableCell>
-            <TableCell align="center" width={1}>
-              Edit
-            </TableCell>
-            <TableCell align="center" width={1}>
-              Remove
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {labelsArray.map((label: string) => (
-            <TableRow key={label}>
-              <TableCell>{label}</TableCell>
-              <TableCell>
-                <IconButton onClick={() => dispatch(editLabel())}>
-                  <EditIcon />
-                </IconButton>
+    <>
+      <Paper>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Label Name</TableCell>
+              <TableCell align="center" width={1}>
+                Edit
               </TableCell>
-              <TableCell>
-                <IconButton onClick={() => dispatch(removeLabel(label))}>
-                  <DeleteIcon />
-                </IconButton>
+              <TableCell align="center" width={1}>
+                Remove
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <TextField
-        fullWidth
-        placeholder="Enter a new label"
-        inputRef={newlabel}
-        slotProps={{
-          input: {
-            startAdornment: (
-              <InputAdornment position="start">
-                <IconButton onClick={handleAddLabel}>
-                  <AddIcon />
-                </IconButton>
-              </InputAdornment>
-            ),
-          },
-        }}
-      />
-    </Paper>
+          </TableHead>
+          <TableBody>
+            {labelsArray.map((label: string) => (
+              <TableRow key={label}>
+                <TableCell>{label}</TableCell>
+                <TableCell>
+                  <IconButton onClick={() => dispatch(editLabel())}>
+                    <EditIcon />
+                  </IconButton>
+                </TableCell>
+                <TableCell>
+                  <IconButton onClick={() => dispatch(removeLabel(label))}>
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TextField
+          fullWidth
+          placeholder="Enter a new label"
+          inputRef={newlabel}
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <IconButton onClick={handleAddLabel}>
+                    <AddIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </Paper>
+    </>
   );
 };
 
