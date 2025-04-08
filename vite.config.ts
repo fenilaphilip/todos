5e98270c4b3980f6222d4a0f827677d502956e36
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-04-08 14:22:10 +0200

    updating vite and adding different favicons

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,32 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: "autoUpdate" })
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Todos",
+        short_name: "Todos",
+        description: "Basically a todo planner App",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    })
   ]
 })
