430f64edf2030b37b171f4b8bb70fdb547322f16
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-04-11 15:24:00 +0200

    hide calender tabs if there is no tasks

--- src/components/dashboard/TaskCalender.tsx
+++ src/components/dashboard/TaskCalender.tsx
@@ -6,19 +6,27 @@ import { Box, Card, Chip, Tab, Tabs, Typography } from "@mui/material";
 import { ViewTasks, TasksView } from "../utils/ViewTasks";
 import Todo from "../../dataModel/todo";
 
-const tabsNamed = ["Upcoming", "Overdue", "Unscheduled"];
+const calenderTabs = ["Upcoming", "Overdue", "Unscheduled"];
 
 export default function TaskCalender() {
   const todoList = useSelector((state: RootState) => state.TODOS);
 
   const navigate = useNavigate();
   const { category } = useParams();
-  const currentCategory = category ?? "Upcoming";
-
-  console.log(`current category ${currentCategory}`);
 
   const { unscheduledTasks, todaysTasks, upcomingTasks, tasksOverdued } =
     getTasksSortedByDate(todoList);
+
+  const taskCounts: {
+    [key: string]: number;
+  } = {
+    Upcoming: todaysTasks.length + upcomingTasks.length,
+    Overdue: tasksOverdued.length,
+    Unscheduled: unscheduledTasks.length,
+  };
+
+  const visibleTabs = calenderTabs.filter((tab) => taskCounts[tab] > 0);
+  const currentCategory = category ?? visibleTabs[0];
 
   let tasksArray: Todo[] = [];
   let taskArrayObj: {
@@ -43,16 +51,8 @@ export default function TaskCalender() {
   }
 
   const handleTabChange = (_event: React.SyntheticEvent, newValue: number) => {
-    console.debug(`switched tab ${tabsNamed[newValue]}`);
-    navigate(`/calenderView/${tabsNamed[newValue]}`);
-  };
-
-  const haveTasks: {
-    [key: string]: number;
-  } = {
-    Upcoming: todaysTasks.length + upcomingTasks.length,
-    Overdue: tasksOverdued.length,
-    Unscheduled: unscheduledTasks.length,
+    // console.debug(`switched tab ${calenderTabs[newValue]}`);
+    navigate(`/calenderView/${visibleTabs[newValue]}`);
   };
 
   if (!todoList.length) {
@@ -67,17 +67,17 @@ export default function TaskCalender() {
     <Box sx={{ width: "100%" }}>
       <Tabs
         component={Card}
-        value={tabsNamed.indexOf(currentCategory)}
+        value={visibleTabs.indexOf(currentCategory)}
         onChange={handleTabChange}
         variant="fullWidth"
         indicatorColor="primary"
         textColor="primary"
       >
-        {tabsNamed.map((tab) => (
+        {visibleTabs.map((tab) => (
           <Tab
             key={tab}
             label={tab}
-            icon={<Chip label={haveTasks[tab]} size="small" color="primary" />}
+            icon={<Chip label={taskCounts[tab]} size="small" color="primary" />}
           />
         ))}
       </Tabs>
