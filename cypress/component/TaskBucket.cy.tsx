2d00a51e3b34d23f2b22454f7cbe4507e966c4d2
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-05-19 16:16:09 +0200

    components test initialized

---
+++ cypress/component/TaskBucket.cy.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import TaskBucket from "../../src/components/dashboard/TaskBucket.tsx";
+
+describe("TaskBucket.cy.tsx", () => {
+  it("playground", () => {
+    // cy.mount()
+  });
+});
